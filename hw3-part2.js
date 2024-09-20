1bcb463840d4c6b59220bebdb26567d7089ecc63
Heri <hrodriguez1821@gmail.com>
2024-09-20 11:20:44 -0700

    Added logic to validate the inputs and display an error message if any input is invalid.

--- hw3-part2.js
+++ hw3-part2.js
@@ -10,4 +10,36 @@ $(document).ready(function() {
             $(this).addClass('nav-active'); // Add the 'active' class
         }
     });
+
+    $("#calculateAmountSold").click(function() {
+        try {
+            const name = document.forms["myform"].elements["sellers-name"].value;
+            validateName(name);
+            const amounts = [];
+            for (let i = 1; i <= 4; i++) {
+                const amount = parseInt(document.forms["myform"].elements[`item-${i}`].value);
+                amounts.push(amount);
+            }
+            validateItemAmounts(amounts);
+        }
+        catch(error) {
+            displayErrorMessage(error.message);
+        }
+    });
+
+    function validateName(name) {
+        if (name === "")
+            throw new Error("The name cannot be empty.");
+    }
+
+    function validateItemAmounts(amounts) {
+        for (amount of amounts) {
+            if (Number.isNaN(amount) || amount < 0)
+                throw new Error("The item amounts need to be a whole number.");
+        }
+    }
+
+    function displayErrorMessage(msg) {
+        alert(msg);
+    }
 });
