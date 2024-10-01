bc38d727f1544457fbcdb53af7a4b9ae52de6355
Heri <hrodriguez1821@gmail.com>
2024-10-01 08:50:20 -0700

    Renamed some variables to give them more descriptive names.

--- hw4-part1.js
+++ hw4-part1.js
@@ -15,16 +15,15 @@ $(document).ready(function() {
         let firstSum = 0;
         let firstProductString = "The result of ";
         let firstProduct = 1;
-        const num1 = 9;
-        const num2 = 25;
-        const step = 4;
-        for (let curr = num1; curr <= num2; curr += step) {
+        const max1 = 25;
+        const step1 = 4;
+        for (let curr = 9; curr <= max1; curr += step1) {
             firstSum += curr;
             firstSumString += curr;
-            firstSumString += (curr < num2)? " + " : ` is ${firstSum}`;
+            firstSumString += (curr < max1)? " + " : ` is ${firstSum}`;
             firstProduct *= curr;
             firstProductString += curr;
-            firstProductString += (curr < num2)? " * " : ` is ${firstProduct.toLocaleString()}`;
+            firstProductString += (curr < max1)? " * " : ` is ${firstProduct.toLocaleString()}`;
         }
         document.getElementById("first-sum-product").innerText = `${firstSumString}\n${firstProductString}`;
 
@@ -33,15 +32,15 @@ $(document).ready(function() {
         let secondProductString = "The result of ";
         let secondProduct = 1;
         let curr = 3;
-        const num4 = 18;
+        const max2 = 18;
         const step2 = 3;
-        while (curr <= num4) {
+        while (curr <= max2) {
             secondSum += curr;
             secondSumString += curr;
-            secondSumString += (curr < num4)? " + " : ` is ${secondSum}`;
+            secondSumString += (curr < max2)? " + " : ` is ${secondSum}`;
             secondProduct *= curr;
             secondProductString += curr;
-            secondProductString += (curr < num4)? " * " : ` is ${secondProduct.toLocaleString()}`;
+            secondProductString += (curr < max2)? " * " : ` is ${secondProduct.toLocaleString()}`;
             curr += step2;
         }
         document.getElementById("second-sum-product").innerText = `${secondSumString}\n${secondProductString}`;
