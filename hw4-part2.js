7ca6e33edc5a602f05b35da4b9cc10db1c3481b3
Heri <hrodriguez1821@gmail.com>
2024-10-03 12:07:38 -0700

    Added logic to create the tables for the different interest rates.

--- hw4-part2.js
+++ hw4-part2.js
@@ -9,4 +9,35 @@ $(document).ready(function() {
             $(this).addClass('nav-active'); // Add the 'active' class
         }
     });
+
+    function addTables() {
+        const section = document.getElementById('hw4-section');
+        const interestRates = [5, 6, 7];
+        const maxYears = 5;
+        for (let table_id = 0; table_id < interestRates.length; table_id++) {
+            section.innerHTML += `
+                <table id="table-${table_id}">
+                    <tr>
+                        <th>Year</th><th>Amount on Deposit</th><th>Interest Rate</th>
+                    </tr>
+                </table>
+            `;
+            const table = document.getElementById(`table-${table_id}`);
+            for (let years = 1; years <= maxYears; years++) {
+                const amount = calculateAmountInSavings(1000, years, interestRates[table_id]).toFixed(2);
+                const interestRate = interestRates[table_id] / 100;
+                table.innerHTML += `
+                    <tr>
+                        <td>${years}</td><td>${amount}</td><td>${interestRate}</td>
+                    </tr>
+                `;
+            }
+        }
+    }
+    
+    function calculateAmountInSavings(princicpal, years, interestRate){
+        return princicpal*(1 + interestRate / 100) ** years;
+    }
+
+    addTables();
 });
