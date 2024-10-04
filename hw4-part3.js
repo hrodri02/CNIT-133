00652bfc547719208fd225aa28e0f740b58ed933
Heri <hrodriguez1821@gmail.com>
2024-10-04 12:59:53 -0700

    Added validation logic and function to reset drawing area when reset button is clicked.

--- hw4-part3.js
+++ hw4-part3.js
@@ -10,19 +10,45 @@ $(document).ready(function() {
         }
     });
 
+    $("#myform").validate({
+        rules: {
+            "side-length": {required: true, number: true, min: 2, max: 10, step: 1}
+        },
+        messages: {
+            "side-length": {
+                required: "Please enter a side length",
+                number: "Please enter a number",
+                min: "The smallest valid side length is 2",
+                max: "The largest valid side length is 10",
+                step: "The side length must be an integer"
+            }
+        },
+        submitHandler: function(form) {
+            getSideLengthAndDrawSquare();
+        }
+    });
+
+    function getSideLengthAndDrawSquare() {
+        const sideLength = parseInt(document.forms["myform"].elements["side-length"].value);
+        drawSquare(sideLength);
+    }
+
     function drawSquare(sideLength) {
         const p = document.getElementById("drawing-area");
+        p.innerText = "";
         for (let i = 0; i < sideLength; i++) {
             if (i === 0 || i === sideLength - 1)
                 p.innerText += "*".repeat(sideLength);
             else {
                 const emptySpace = "\u00A0".repeat((sideLength - 2) * 2);
-                console.log(emptySpace.length);
                 p.innerText += ("*" + emptySpace + "*");
             }
             p.innerText += "\n";
         }
     }
 
-    drawSquare(10);
+    $("#resetDrawingArea").click(function() {
+        const p = document.getElementById("drawing-area");
+        p.innerText = "";
+    });
 });
