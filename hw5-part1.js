29ab4ed0f938cbaddd97aa5ff967d2d4624d346b
Heri <hrodriguez1821@gmail.com>
2024-10-16 09:09:53 -0700

    Added the logic to validate user input and display an appropriate error message or to display a success message if all data is valid.

--- hw5-part1.js
+++ hw5-part1.js
@@ -9,4 +9,43 @@ $(document).ready(function() {
             $(this).addClass('nav-active'); // Add the 'active' class
         }
     });
+
+    $("#submitUserData").click(function() {
+        const textarea = document.getElementById("result");
+        try {
+            validateUserData();
+            textarea.innerText = "Thanks, your data was submitted!";
+            document.forms["myform"].reset();
+        } 
+        catch (error) {
+            textarea.innerText = error.message;
+        }
+    });
+
+    function validateUserData() {
+        const fullname = document.forms["myform"].elements["fullname"].value;
+        if (fullname.length === 0) {
+            throw new Error("Full name is required");
+        }
+        
+        const lessThan21 = document.getElementById("less-than-21");
+        const between21and50 = document.getElementById("21-50");
+        const olderThan50 = document.getElementById("older-than-50");
+        if (!(lessThan21.checked || between21and50.checked || olderThan50.checked)) {
+            throw new Error("Age group is required");
+        }
+
+        const chrome = document.getElementById("chrome");
+        const firefox = document.getElementById("firefox");
+        const edge = document.getElementById("edge");
+        const safari = document.getElementById("safari");
+        if (!(chrome.checked || firefox.checked || edge.checked || safari.checked)) {
+            throw new Error("You must select at least one browser");
+        }
+
+        const selectedGenre = document.forms["myform"].elements["movie-genres"].value;
+        if (selectedGenre === "") {
+            throw new Error("You must select your favorite movie genre.");
+        }
+    }
 });
