7553202915a3e5285c8888aa8cf85afbf866bac7
Heri <hrodriguez1821@gmail.com>
2024-10-16 10:01:03 -0700

    Added logic to open selected destinations in a new tab.

--- hw5-part2.js
+++ hw5-part2.js
@@ -9,4 +9,16 @@ $(document).ready(function() {
             $(this).addClass('nav-active'); // Add the 'active' class
         }
     });
+
+    $("#go").click(function() {
+        const urlString = document.forms["form2"].elements["destList2"].value;
+        if (urlString !== "")
+            window.open(urlString, "_blank");
+    });
+
+    $("#destList1").change(function() {
+        const urlString = $(this).children("option:selected").val();
+        if (urlString !== "")
+            window.open(urlString, "_blank");
+    });
 });
