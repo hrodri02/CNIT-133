49fc4df975f85fb63fd4ce60f7ca69c7b2d537a4
Heri <hrodriguez1821@gmail.com>
2024-10-17 13:51:52 -0700

    Moved js code to separate file and added logic to hide/show vertical nav bar.

---
+++ hw2-part1.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    // update the selected page in the nav bar
+    const currentPath = window.location.pathname;
+    const parts = currentPath.split('/');
+    const currentPage = parts[parts.length - 1];
+    // Iterate over each <a> tag 
+    $("nav").children('a').each(function () {
+        // Check if the href attribute matches the current path
+        if ($(this).attr('href') === currentPage) {
+            $(this).addClass('nav-active'); // Add the 'active' class
+        }
+    });
+
+    $("#menu-bar").click(function() {
+        $(".vertical-nav-bar").toggle();
+    });
+});
