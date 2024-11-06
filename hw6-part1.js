5623a43aa414fe597f203ed9b6165bb8072a3eab
Heri <hrodriguez1821@gmail.com>
2024-11-06 09:29:17 -0800

    Basic layout and ui for hw 6 part 1.

---
+++ hw6-part1.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    const currentPath = window.location.pathname;
+    const parts = currentPath.split('/');
+    const currentPage = parts[parts.length - 1];
+    // Iterate over each <a> tag 
+    $("nav").children('a').each(function () {
+        // Check if the href attribute matches the current path
+        if ($(this).attr('href') === currentPage) {
+            $(this).addClass('nav-active'); // Add the 'active' class
+        }
+    });
+
+    $("#menu-bar").click(function() {
+        $(".vertical-nav-bar").toggle();
+    });
+});
