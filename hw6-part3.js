df61ed38dd15f45cd79efad3471fc317e99eb84f
Heri <hrodriguez1821@gmail.com>
2024-11-09 10:38:55 -0800

    Added the logic to input and validate a phone number as well as display an appropriate message.

--- hw6-part3.js
+++ hw6-part3.js
@@ -13,4 +13,37 @@ $(document).ready(function() {
     $("#menu-bar").click(function() {
         $(".vertical-nav-bar").toggle();
     });
+
+    $.validator.addMethod("regex", function(value, element, regexp) {
+        return this.optional(element) || regexp.test(value);
+    }, "Please enter a valid value.");
+
+    $("#myform").validate({
+        rules: {
+            "phone-number": {
+                required: true,
+                regex: /\((\d){3,3}\) (\d){3,3}-(\d){4,4}/
+            }
+        },
+        messages: {
+            "phone-number": {
+                required: "Pleae enter your phone number.",
+                regex: "The phone number must be in the format (XXX) XXX-XXXX."
+            }
+        },
+        submitHandler: function(form) {
+            displayResults();
+        },
+        errorPlacement: function (error, element) {
+            $("#result").val(error.text());
+        }
+    });
+
+    function displayResults() {
+        $("#result").val("Thank you for submitting your phone number. One of our agents will contact you within a week.");
+    }
+
+    $("#reset").click(function() {
+        $("#result").val("");
+    });
 });
