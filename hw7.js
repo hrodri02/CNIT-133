193f5424e49eb0bf00016884f12ca3cb1d662c63
Heri <hrodriguez1821@gmail.com>
2024-11-27 09:27:42 -0800

    Modified hw 7 home to include drop downs for the description of each part.

--- hw7.js
+++ hw7.js
@@ -6,14 +6,14 @@ $(document).ready(function() {
         document.getElementsByClassName("content-container")[0].innerHTML += `
             <main>
                 <h3>Welcome to my implementation of HW 7 for my Javascript, jQuery, and AJAX class.</h3>
-                <ol>
-                    <li>
-                        Part 1 provides a form to change the styles of three paragraphs.
-                    </li>
-                    <li>
-                        Part 2 contains a horizonal navigation bar with drop down menus.
-                    </li>
-                </ol>
+                <h4>Part 1</h4>
+                <div class="description">
+                    <a href="hw7-part1.html">Part 1</a> provides a form to change the styles of three paragraphs.
+                </div>
+                <h4>Part 2</h4>
+                <div class="description">
+                    <a href="hw7-part2.html">Part 2</a> contains a horizonal navigation bar with drop down menus.
+                </div>    
             </main>
         `;
     }
@@ -30,6 +30,16 @@ $(document).ready(function() {
         // Check if the href attribute matches the current path
         if ($(this).attr('href') === currentPage) {
             $(this).addClass('nav-active'); // Add the 'active' class
+        }
+    });
+
+    $('.description').hide();
+    $('main h4').click(function() {
+        const description = $(this).next('.description');
+        if (description.is(':visible')) {
+            description.slideUp(200);
+        } else {
+            description.slideDown(200);
         }
     });
 });
