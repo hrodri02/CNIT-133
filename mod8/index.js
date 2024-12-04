86d41205166332dca51750b6c1b90136123a58a1
Heri <hrodriguez1821@gmail.com>
2024-12-04 10:55:18 -0800

    Use jquery with ajax instead of pure javascript to make http get request.

--- mod8/index.js
+++ mod8/index.js
@@ -1,30 +1,38 @@
-function ajaxFunction(){
-	let ajaxRequest;  // The variable that makes Ajax possible!
-	
-	try{
-		// Opera 8.0+, Firefox, Safari
-		ajaxRequest = new XMLHttpRequest();
-	} catch (e){
-		// Internet Explorer Browsers
+$(document).ready(function() {
+	function ajaxFunction() {
+		let ajaxRequest;  // The variable that makes Ajax possible!
+		
 		try{
-			ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP");
-		} catch (e) {
+			// Opera 8.0+, Firefox, Safari
+			ajaxRequest = new XMLHttpRequest();
+		} catch (e){
+			// Internet Explorer Browsers
 			try{
-				ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
-			} catch (e){
-				// Something went wrong
-				alert("Your browser broke!");
-				return false;
+				ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP");
+			} catch (e) {
+				try{
+					ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
+				} catch (e){
+					// Something went wrong
+					alert("Your browser broke!");
+					return false;
+				}
 			}
 		}
+	
+		ajaxRequest.onreadystatechange = function(){
+			if(ajaxRequest.readyState == 4){
+				// Get the data from the server's response
+				document.myform.time.value = ajaxRequest.responseText;
+			}
+		}
+		ajaxRequest.open("GET", "serverTime.php", true);
+		ajaxRequest.send();
 	}
 
-    ajaxRequest.onreadystatechange = function(){
-        if(ajaxRequest.readyState == 4){
-            // Get the data from the server's response
-            document.myform.time.value = ajaxRequest.responseText;
-        }
-    }
-    ajaxRequest.open("GET", "serverTime.php", true);
-    ajaxRequest.send();
-}
+	$("#fullname").change(function() {
+		$.get("serverTime.php", function(data) {
+			document.myform.time.value = data;
+		});
+	});
+});
